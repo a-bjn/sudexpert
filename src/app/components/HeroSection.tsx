46b7c062ba560b0c12784b3d12bd8f159a2c603b
a-bjn <andreibejan55@yahoo.com>
2024-12-06 18:09:10 +0200

    add email form to hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
   }, []);
 
   return (
-    <section className="relative w-full h-screen">
+    <section className="relative w-full h-screen-min">
       {/* Video Background */}
       <video
         autoPlay
@@ -54,11 +54,55 @@ export default function HeroSection() {
           {/* Button */}
           <a
             href="tel:+40724207132"
-            className="block w-full text-center px-8 py-3 border border-white text-white rounded-full text-lg hover:bg-white hover:text-black transition-all duration-300 max-w-md shadow-[0px_10px_30px_rgba(0,0,0,0.8)]"
+            className="block w-full text-center px-8 py-3 mt-10 border border-white text-white rounded-full text-lg hover:bg-white hover:text-black transition-all duration-300 max-w-md shadow-[0px_10px_30px_rgba(0,0,0,0.8)]"
           >
             +40 724 207 132
           </a>
-
+          {/* Email Form */}
+          <div className="bg-gray-100/10 p-6 rounded-2xl mt-12 mb-12">
+            <div className="w-full max-w-lg">
+                <h2 className="text-2xl font-semibold text-center mb-6"> Ne puteti contacta si prin Email </h2>
+                <form className="space-y-4">
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-teal-300"> Nume </label>
+                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Numele"/>
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-teal-300"> Prenume </label>
+                        <input type="email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-400" placeholder="Prenumele"/>
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-teal-300"> Telefon* </label>
+                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Numar de telefon"
+                        />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-teal-300"> Email* </label>
+                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Adresa dv. de email"
+                        />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-teal-300"> Subiect </label>
+                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Subiect (ex. comanda sarma)"
+                        />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-teal-300"> Mesaj* </label>
+                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Mesajul dv."
+                        />
+                    </div>
+                    <div className="flex items-center">
+                      <input type="checkbox" className="h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                        <label className="ml-2 text-sm text-white"> Sunt de acord cu termenii si conditiile </label>
+                    </div>
+                    <div>
+                        <button type="submit" className="w-full px-4 py-2 mt-4 text-white bg-teal-700 rounded-md hover:bg-teal-800 focus:outline-none focus:bg-teal-800">
+                          Trimite mail-ul
+                        </button>
+                    </div>
+                </form>
+            </div>
+          </div>
           {/* Trusted By Section */}
           <div className="flex flex-col items-center gap-2 mt-4">
             <p className="text-md sm:text-lg text-white text-center">
