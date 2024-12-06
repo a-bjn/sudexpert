016790d7ba357b4522203e8d2d51edc3b4467558
a-bjn <andreibejan55@yahoo.com>
2024-12-06 18:37:39 +0200

    change herosection layout

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -9,8 +9,16 @@ export default function HeroSection() {
     AOS.init({ duration: 800, once: true });
   }, []);
 
+  const [message, setMessage] = useState("");
+
+  const handleInputChange = (e: { target: { value: SetStateAction<string>; style: { height: string; }; scrollHeight: any; }; }) => {
+    setMessage(e.target.value);
+    e.target.style.height = 'auto';
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
+  
   return (
-    <section className="relative w-full h-screen-min">
+    <section className="relative w-full h-[238vh]">
       {/* Video Background */}
       <video
         autoPlay
@@ -46,49 +54,64 @@ export default function HeroSection() {
         </div>
 
         {/* Call to Action Section */}
-        <div
-          className="flex flex-col justify-between items-center w-full"
-          data-aos="fade-up"
-          data-aos-delay="200"
-        >
+        <div className="flex flex-col justify-between items-center w-full" data-aos="fade-up" data-aos-delay="200">
           {/* Button */}
           <a
             href="tel:+40724207132"
-            className="block w-full text-center px-8 py-3 mt-10 border border-white text-white rounded-full text-lg hover:bg-white hover:text-black transition-all duration-300 max-w-md shadow-[0px_10px_30px_rgba(0,0,0,0.8)]"
+            className="block w-full text-center px-8 py-3 border border-white text-white rounded-full text-lg hover:bg-white hover:text-black transition-all duration-300 max-w-md shadow-[0px_10px_30px_rgba(0,0,0,0.8)]"
           >
             +40 724 207 132
           </a>
-          {/* Email Form */}
-          <div className="bg-gray-100/10 p-6 rounded-2xl mt-12 mb-12">
+          
+          {/* Trusted By Section */}
+          <div className="flex flex-col items-center gap-2 mt-6">
+            <p className="text-md sm:text-lg text-white text-center">
+              Asigurăm încrederea mărcilor de top din industrie.
+            </p>
+            <div className="flex gap-1 text-1xl text-teal-400">
+              {"★ ★ ★ ★ ★".split(" ").map((star, index) => (
+                <span key={index}>{star}</span>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Email Form */}
+        <div className="bg-gray-100/10 p-6 rounded-2xl">
             <div className="w-full max-w-lg">
                 <h2 className="text-2xl font-semibold text-center mb-6"> Ne puteti contacta si prin Email </h2>
                 <form className="space-y-4">
                     <div>
                         <label className="block mb-2 text-sm font-medium text-teal-300"> Nume </label>
-                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Numele"/>
+                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-800 text-black" placeholder="Numele"/>
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-teal-300"> Prenume </label>
-                        <input type="email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-400" placeholder="Prenumele"/>
+                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-800 text-black" placeholder="Prenumele"/>
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-teal-300"> Telefon* </label>
-                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Numar de telefon"
+                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-800 text-black" placeholder="Numar de telefon"
                         />
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-teal-300"> Email* </label>
-                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Adresa dv. de email"
+                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-800 text-black" placeholder="Adresa dv. de email"
                         />
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-teal-300"> Subiect </label>
-                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Subiect (ex. comanda sarma)"
+                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-800 text-black" placeholder="Subiect (ex. comanda sarma)"
                         />
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-teal-300"> Mesaj* </label>
-                        <input type="password" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Mesajul dv."
+                        <textarea
+                          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-800 text-black resize-none overflow-auto"
+                          placeholder="Mesajul dv."
+                          rows={4}
+                          value={message}
+                          onChange={handleInputChange} // Handle input change to auto-resize
                         />
                     </div>
                     <div className="flex items-center">
@@ -103,18 +126,6 @@ export default function HeroSection() {
                 </form>
             </div>
           </div>
-          {/* Trusted By Section */}
-          <div className="flex flex-col items-center gap-2 mt-4">
-            <p className="text-md sm:text-lg text-white text-center">
-              Asigurăm încrederea mărcilor de top din industrie.
-            </p>
-            <div className="flex gap-1 text-1xl text-teal-400">
-              {"★ ★ ★ ★ ★".split(" ").map((star, index) => (
-                <span key={index}>{star}</span>
-              ))}
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   );
